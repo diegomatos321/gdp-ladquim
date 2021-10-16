062637579a3b3133a2b07f285620e0b55828548f
diegomatos <diegovasconcelos62@gmail.com>
2021-10-16 10:18:14 -0300

    Instantiate Phaser Game

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import Phaser from "phaser"
+import MainGame from "./scenes/MainGame.js"
+
+let config = {
+  title: "LADQUIM - Conservação e Restauração",
+  version: "1.0.0",
+  width: 1280,
+  height: 768,
+  parent: "game-container",
+  type: Phaser.AUTO,
+  backgroundColor: "#00b3ff",
+  dom: {
+    createContainer: true
+  },
+  scale: {
+    width: 1280,
+    height: 768,
+    parent: "game-container",
+    fullscreenTarget: "game-container",
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  autoFocus: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 1200 },
+      debug: true
+    }
+  },
+  banner: true,
+  url: "https://ladquim.iq.ufrj.br/",
+  scene: [MainGame]
+};
+
+let game = new Phaser.Game(config);
