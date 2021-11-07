b31dc8a15803bd365da00f06080ea57828db0e78
Diego Matos <diegovasconcelos62@gmail.com>
2021-11-07 18:14:44 -0300

    Implementando o Menu

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -16,8 +16,9 @@ export default class MenuScene extends Phaser.Scene {
     this.load.image("menu-title", new URL("./images/menu-title.png", import.meta.url).pathname);
 
     this.load.html("ladquim-mapa", new URL("./DOMElements/mapa-laquim.html", import.meta.url).pathname);
-    this.load.spritesheet("fullscreen-icon", new URL("../ui/fullscreen.png", import.meta.url).pathname, {      frameWidth: 64,
-      frameHeight: 64
+    this.load.spritesheet("fullscreen-icon", new URL("../ui/fullscreen.png", import.meta.url).pathname, {
+      frameWidth: 164,
+      frameHeight: 164
     });
   }
 
@@ -28,16 +29,22 @@ export default class MenuScene extends Phaser.Scene {
     const label = ["O Projeto", "Leaderboard", "Créditos", "Configurações"]
     const initX = 1440, initY = 400, stepY = 150;
     for (let index = 0; index < 4; index++) {
-      const botao = this.add.image(initX + (350/2), (400 + 97/2) + (index * stepY), "menu-botao");
-      this.add.text(botao.x, botao.y, label[index], {fontSize: "34px"}).setOrigin(0.5, 0.5);
+      const botao = this.add.image(initX + (350/2), (400 + 97/2) + (index * stepY), "menu-botao").setInteractive();
+      botao.on(Phaser.Input.Events.POINTER_OVER, () => botao.setTexture("menu-botao-hover"))
+      botao.on(Phaser.Input.Events.POINTER_OUT, () => botao.setTexture("menu-botao"))
+
+      this.add.text(botao.x, botao.y, label[index], {fontFamily: "Nunito ExtraBold", fontSize: "43px"}).setOrigin(0.5, 0.5);
     }
+
+    this.add.image(683, 197, "menu-title");
     
-    this.ladquimArea = this.createLadquimMap();
+    this.ladquimArea = this.createLadquimMap(683, 699);
+    this.add.text(this.ladquimArea.x, 1030, "Selecione um Mini-Jogo!", {fontFamily: "Nunito ExtraBold", fontSize: "43px"}).setOrigin(0.5, 0.5);
     fullScreenBtnComponent(this);
   }
   
-  createLadquimMap() {
-    let ladquimArea = this.add.dom(this.game.config.width / 2, this.game.config.height / 2).createFromCache("ladquim-mapa");
+  createLadquimMap(x, y) {
+    let ladquimArea = this.add.dom(x, y).createFromCache("ladquim-mapa");
     ladquimArea.addListener(Phaser.Input.Events.POINTER_UP);
     ladquimArea.on(Phaser.Input.Events.POINTER_UP, this.changeScene);
 
