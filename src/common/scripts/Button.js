8c8618f1b809652736c91a7cb60ed6facbdea6fa
diegomatos <diegovasconcelos62@gmail.com>
2021-11-13 17:17:23 -0300

    Adaptando Script do Botão

--- src/common/scripts/Button.js
+++ src/common/scripts/Button.js
@@ -3,24 +3,14 @@ export default class Button extends Phaser.GameObjects.Container {
     super(scene, x, y);
 
     scene.add.existing(this)
-
-    let botao = scene.add.rexNinePatch({
-      x: 0, 
-      y: 0, 
-      width: 350, 
-      height: 97, 
-      key: "menu-atlas", 
-      baseFrame: "menu-botao", 
-      columns: [6, undefined, 6], 
-      rows: [6, undefined, 6]
-    })
-    this.add(botao)
+    let textura = scene.add.image(0, 0, "menu-atlas", "menu-botao")
+    this.add(textura)
     
     const buttonText = scene.add.text(0, 0, text, textConfig).setOrigin(0.5);
     this.add(buttonText)
     
-    this.setInteractive(new Phaser.Geom.Rectangle(-botao.width/2, -botao.height/2, botao.width, botao.height), Phaser.Geom.Rectangle.Contains);
-    this.on(Phaser.Input.Events.POINTER_OVER, () => botao.setTexture("menu-atlas", "menu-botao-hover", [6, undefined, 6], [6, undefined, 6]))
-    this.on(Phaser.Input.Events.POINTER_OUT, () => botao.setTexture("menu-atlas", "menu-botao", [6, undefined, 6], [6, undefined, 6]))
+    this.setInteractive(new Phaser.Geom.Rectangle(-textura.displayWidth/2, -textura.displayHeight/2, textura.displayWidth, textura.displayHeight), Phaser.Geom.Rectangle.Contains);
+    this.on(Phaser.Input.Events.POINTER_OVER, () => textura.setTexture("menu-atlas", "menu-botao-hover", [6, undefined, 6], [6, undefined, 6]))
+    this.on(Phaser.Input.Events.POINTER_OUT, () => textura.setTexture("menu-atlas", "menu-botao", [6, undefined, 6], [6, undefined, 6]))
   }
 }
