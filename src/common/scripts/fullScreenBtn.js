cb18c7abdeb82e3f4ed327fddc48b9e50fb5ded8
diegomatos <diegovasconcelos62@gmail.com>
2021-11-29 20:44:22 -0300

    Alterando textura ao clica no botão de tela cheia

--- src/common/scripts/fullScreenBtn.js
+++ src/common/scripts/fullScreenBtn.js
@@ -2,17 +2,18 @@ export default class FullScreenBtn extends Phaser.GameObjects.Image {
   constructor(scene) {
     super(scene, scene.game.config.width - 16, 16, "common-atlas", "fullscreen");
 
-    scene.add.existing(this)
+    this.scene.add.existing(this)
 
     this.setOrigin(1, 0);
     this.setInteractive();
 
-    this.on(Phaser.Input.Events.POINTER_UP, this.handleFullScreenMode);
+    this.on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handleFullScreenMode);
     this.on(Phaser.Scale.Events.FULLSCREEN_UNSUPPORTED, this.handleFullScreenUnsupported);
     this.scene.events.on(Phaser.Scenes.Events.SHUTDOWN, this.cleanEvents)
   }
 
   handleFullScreenMode = () => {
+    this.scene.scale.isFullscreen ? this.setTexture("common-atlas", "fullscreen") : this.setTexture("common-atlas", "fullscreen-clicked")
     this.scene.scale.toggleFullscreen();
   }
 
@@ -22,7 +23,7 @@ export default class FullScreenBtn extends Phaser.GameObjects.Image {
 
   cleanEvents = () => {
     console.log("Cleaning events from FullScreenBtn")
-    this.removeListener(Phaser.Input.Events.POINTER_UP, this.handleFullScreenMode);
+    this.removeListener(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handleFullScreenMode);
     this.removeListener(Phaser.Scale.Events.FULLSCREEN_UNSUPPORTED, this.handleFullScreenUnsupported)
   }
 }
