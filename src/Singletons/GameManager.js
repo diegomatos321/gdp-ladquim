d25f2594b2ab97b94f61106c87936f8261c0a77b
diegomatos <diegovasconcelos62@gmail.com>
2021-12-03 15:40:51 -0300

    Extraindo método para Classe

--- src/Singletons/GameManager.js
+++ src/Singletons/GameManager.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser"
+import CheckOrientation from "../common/scripts/CheckOrientation"
 import CONSTANTS from "../constants.json"
 
 export default class GameManager extends Phaser.Scene {
@@ -13,32 +14,9 @@ export default class GameManager extends Phaser.Scene {
   create() {
     this.scene.bringToTop()
     
-    this.CheckOrientation();
+    new CheckOrientation(this, this.game.config.width/2, 60);
     this.HandlePauseLogic();
     this.events.on(Phaser.Scenes.Events.SHUTDOWN, this.cleanEvents)
-  }
-
-  CheckOrientation = () => {
-    const textStyle = {
-      fontFamily: "Nunito-Black", 
-      fontSize: "43px", 
-      backgroundColor: "#ad1403", 
-      padding: 30
-    }
-    this.orientationText = this.add.text(this.game.config.width/2, 60, "Vire o seu celular na Horizontal", textStyle).setOrigin(0.5).setVisible(false);
-
-    this.handleChangeOrientation(this.scale.orientation);
-    this.scale.on(Phaser.Scale.Events.ORIENTATION_CHANGE, this.handleChangeOrientation);
-  }
-
-  handleChangeOrientation = (scaleOrientation) => {
-    if (scaleOrientation === Phaser.Scale.PORTRAIT) {
-      console.log("PORTRAIT");
-      this.orientationText.setVisible(true);
-    } else if (scaleOrientation === Phaser.Scale.LANDSCAPE) {
-      console.log("LANDSCAPE");
-      this.orientationText.setVisible(false);
-    }
   }
 
   HandlePauseLogic = () => {
---
+++ src/common/scripts/CheckOrientation.js
@@ -0,0 +1,38 @@
+import Phaser from "phaser";
+
+const text = "Vire o seu celular na Horizontal";
+const textStyle = {
+  fontFamily: "Nunito-Black", 
+  fontSize: "43px", 
+  backgroundColor: "#ad1403", 
+  padding: 30
+};
+
+export default class CheckOrientation extends Phaser.GameObjects.Text{
+  constructor(scene, x, y) {
+    super(scene, x, y, text, textStyle)
+
+    this.scene.add.existing(this)
+
+    this.setOrigin(0.5);
+    this.setVisible(false);
+
+    this.handleChangeOrientation(this.scene.scale.orientation);
+    this.scene.scale.on(Phaser.Scale.Events.ORIENTATION_CHANGE, this.handleChangeOrientation);
+    this.scene.events.on(Phaser.Scenes.Events.SHUTDOWN, this.cleanEvents)
+  }
+
+  handleChangeOrientation = (scaleOrientation) => {
+    if (scaleOrientation === Phaser.Scale.PORTRAIT) {
+      console.log("PORTRAIT");
+      this.setVisible(true);
+    } else if (scaleOrientation === Phaser.Scale.LANDSCAPE) {
+      console.log("LANDSCAPE");
+      this.setVisible(false);
+    }
+  }
+
+  cleanEvents = (sys) => {
+    sys.scene.scale.removeListener(Phaser.Scale.Events.ORIENTATION_CHANGE, sys.scene.handleChangeOrientation);
+  }
+}
