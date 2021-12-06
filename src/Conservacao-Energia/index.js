f521340bd723afc96e98957d6f5802848c723ab2
David Albuquerque <davidra_rj@hotmail.com>
2021-12-05 23:06:59 -0300

    wip: random rain generation

--- src/Conservacao-Energia/index.js
+++ src/Conservacao-Energia/index.js
@@ -7,94 +7,78 @@ import GameTimer from "./prefabs/GameTimer"
 import FinishGame from "../common/scripts/FinishGame"
 import LoadingInterface from "../common/scripts/LoadingInterface"
 import ConservacaoPauseScene from "./components/ConservacaoPauseScene"
+import Rain from "./prefabs/Rain"
 
 export default class ConservacaoEnergiaScene extends Phaser.Scene {
   constructor() {
     super({ key: CONSTANTS.MINI_GAME_QUIMICA_CONSERVACAO });
 
-    var gameTimer
-    var pauseGame
-
+    var rainSources
+    var grupoDeMesas
+    var grupoDeAreasDeEfeito
   }
 
   preload = () => {
     new LoadingInterface(this, this.game.config.width / 2, this.game.config.height / 2)
 
     this.loadImages();
-
-    this.load.image('left-cap', new URL("./images/uipack-space/barHorizontal_green_left.png", import.meta.url).pathname)
-    this.load.image('middle', new URL("./images/uipack-space/barHorizontal_green_mid.png", import.meta.url).pathname)
-    this.load.image('right-cap', new URL("./images/uipack-space/barHorizontal_green_right.png", import.meta.url).pathname)
-
-    this.load.image('left-cap-shadow', new URL("./images/uipack-space/barHorizontal_shadow_left.png", import.meta.url).pathname)
-    this.load.image('middle-shadow', new URL("./images/uipack-space/barHorizontal_shadow_mid.png", import.meta.url).pathname)
-    this.load.image('right-cap-shadow', new URL("./images/uipack-space/barHorizontal_shadow_right.png", import.meta.url).pathname)
   }
 
   create = () => {
+
+    //Adicionando cenas extras
     ConservacaoPauseScene.LoadPauseScene(this)
+
+    //Carregando variáveis
+    this.grupoDeAreasDeEfeito = this.physics.add.staticGroup();
+    this.rainSources = []
+
+    //Adicionando background
+    this.add.image(this.game.config.width/2, this.game.config.height/2,"background")
+
     // Configurando bordas de colisoes do mundo
     this.physics.world.setBounds(0, 0, this.game.config.width, this.game.config.height);
 
     this.gameTimer = new GameTimer(this, 240, 36)
 
     // Grupo estatico de mesas
-    let grupoDeMesas = this.physics.add.staticGroup({ classType: Mesa });
-    grupoDeMesas.get(200, this.game.config.height - 100);
-    grupoDeMesas.get(this.game.config.width - 200, this.game.config.height - 100);
+    this.grupoDeMesas = this.physics.add.staticGroup({ classType: Mesa });
+    this.grupoDeMesas.get(200, this.game.config.height - 100);
+    this.grupoDeMesas.get(this.game.config.width - 200, this.game.config.height - 100);
 
     // Grupo de vasos
     let grupoDeItems = this.physics.add.group({ collideWorldBounds: true });
 
     // Criando vasos
     for (let index = 0; index < 1; index++) {
-      const mesa = grupoDeMesas.getFirstAlive();
+      const mesa = this.grupoDeMesas.getFirstAlive();
       const stepX = (mesa.displayWidth / 2 * index);
 
       let vasoAntigo = new VasoAntigo(this, (mesa.x - mesa.displayWidth / 4) + stepX, this.game.config.height / 2);
       grupoDeItems.add(vasoAntigo, true);
     };
-
-    // Raindrop particles
-    const target1 = grupoDeMesas.getFirstAlive();
-    let rainSource = new Phaser.Geom.Line(target1.x - target1.width / 2, 0, target1.x + target1.width / 2, 0);
-
-    let raindropParticles = this.add.particles("raindrop");
-    raindropParticles.createEmitter({
-      speedY: 300,
-      gravityY: this.game.config.physics.arcade.gravity.y,
-      lifespan: 1000,
-      quantity: 10,
-      frequency: 20,
-      rotate: -15,
-      emitZone: {
-        source: rainSource,
-        type: "random"
-      },
-    });
-
-    let rainHitArea = this.createRainHitArea(rainSource);
-
-    // Grupo de áreas de efeito
-    let grupoDeAreasDeEfeito = this.physics.add.staticGroup();
-    grupoDeAreasDeEfeito.add(rainHitArea, true);
-
+    
     // Colisoes
-    this.physics.add.collider(grupoDeItems, grupoDeMesas);
+    this.physics.add.collider(grupoDeItems, this.grupoDeMesas);
 
     // Overlap
-    this.physics.add.overlap(grupoDeItems, grupoDeMesas, this.repositionVase);
-    this.physics.add.overlap(grupoDeItems, grupoDeAreasDeEfeito, this.damageItem);
+    this.physics.add.overlap(grupoDeItems, this.grupoDeMesas, this.repositionVase);
+    this.physics.add.overlap(grupoDeItems, this.grupoDeAreasDeEfeito, this.damageItem);
 
     this.gameTimer = new GameTimer(this, 240, 36)
   }
 
   update = () => {
     this.gameTimer.updateTimer()
+    
     if (this.gameTimer.hasEnded) {
       FinishGame.FinishToMainMenu(this)
     }
-
+    
+    this.rainSources.forEach((e) => {
+      e.updateRain();
+    })
+    this.generateRandomRainArea();
   }
 
   /**
@@ -106,6 +90,16 @@ export default class ConservacaoEnergiaScene extends Phaser.Scene {
     this.load.image("vaso", new URL("./images/vaso-grego-antigo.png?quality=75&width=75", import.meta.url).pathname);
     this.load.image("mesa", new URL("./images/desk-sprite.png?quality=75&width=300", import.meta.url).pathname);
     this.load.image("raindrop", new URL("./images/raindrop-2d-sprite.png?quality=75&width=8", import.meta.url).pathname);
+
+    this.load.image('background', new URL("./images/background.jpg", import.meta.url).pathname)
+
+    this.load.image('left-cap', new URL("./images/uipack-space/barHorizontal_green_left.png", import.meta.url).pathname)
+    this.load.image('middle', new URL("./images/uipack-space/barHorizontal_green_mid.png", import.meta.url).pathname)
+    this.load.image('right-cap', new URL("./images/uipack-space/barHorizontal_green_right.png", import.meta.url).pathname)
+
+    this.load.image('left-cap-shadow', new URL("./images/uipack-space/barHorizontal_shadow_left.png", import.meta.url).pathname)
+    this.load.image('middle-shadow', new URL("./images/uipack-space/barHorizontal_shadow_mid.png", import.meta.url).pathname)
+    this.load.image('right-cap-shadow', new URL("./images/uipack-space/barHorizontal_shadow_right.png", import.meta.url).pathname)
   }
 
   createRainHitArea = (rainSource) => {
@@ -114,6 +108,18 @@ export default class ConservacaoEnergiaScene extends Phaser.Scene {
     let rainHitArea = this.add.rectangle(rainSource.x1 + widthOfRainHitArea / 2, rainSource.y1 + heightOfRainHitArea / 2, widthOfRainHitArea, heightOfRainHitArea);
     rainHitArea.setData("power", 0.1);
     return rainHitArea;
+  }
+
+  generateRandomRainArea = () => {
+    let randomNumber = Phaser.Math.Between(0, 100);
+
+    if (randomNumber < 1) {
+      const target = this.grupoDeMesas.getFirstAlive();
+      let rainSource = new Rain(target, this, "raindrop");
+      Rain.CreateEmitter(rainSource.raindropParticles, rainSource, this)
+      this.rainSources.push(rainSource)
+      this.grupoDeAreasDeEfeito.add(rainSource.rainHitArea, true);
+    }
   }
 
   repositionVase = (item, mesa) => {
