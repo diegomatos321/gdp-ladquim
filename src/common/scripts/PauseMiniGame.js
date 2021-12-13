686ab2e18aae96b22a7788e79465d3b40dcbb4d1
diegomatos <diegovasconcelos62@gmail.com>
2021-12-12 23:31:23 -0300

    Generalizando Pause Container

--- src/common/scripts/PauseMiniGame.js
+++ src/common/scripts/PauseMiniGame.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import ConfiguracoesContainer from "../modals/ConfiguracoesContainer";
 import CONSTANTS from "../../constants.json"
 
 export default class PauseMiniGameContainer extends Phaser.GameObjects.Container {
@@ -8,14 +9,25 @@ export default class PauseMiniGameContainer extends Phaser.GameObjects.Container
     this.scene.add.existing(this);
 
     this.setData("Blacklist", new Map([
-      [CONSTANTS.MAIN_MENU, "Menu Principal"]
+      [CONSTANTS.MAIN_MENU, "Menu Principal"],
+      [CONSTANTS.INICIO_MINI_GAME_QUIMICA_CONSERVACAO, "Inicio Conservacao Energia"],
+      [CONSTANTS.FIM_MINI_GAME_QUIMICA_CONSERVACAO, "Fim Conservacao Energia"]
     ]));
     
-    this.pauseImage = this.scene.add.image(0, 0, "pauseImage").setVisible(false);
-    this.add([this.pauseImage]);
+    // this.pauseImage = this.scene.add.image(0, 0, "pauseImage").setVisible(false);
+    // this.add([this.pauseImage]);
 
-    this.scene.input.keyboard.on("keyup-" + "W", this.pauseCurrentMiniGame);
+    this.pauseModal = new ConfiguracoesContainer(this.scene).setVisible(false);
+    this.add(this.pauseModal)
+    
+    this.scene.input.keyboard.on("keyup-" + "W", this.emitEvent);
+    this.scene.events.on(CONSTANTS.PAUSED, this.pauseCurrentMiniGame);
+    this.pauseModal.on(CONSTANTS.BACK_ARROW_CLICKED, this.emitEvent)
     this.scene.events.on(Phaser.Scenes.Events.SHUTDOWN, this.cleanEvents);
+  }
+
+  emitEvent = () => {
+    this.scene.events.emit(CONSTANTS.PAUSED)
   }
 
   pauseCurrentMiniGame = () => {
@@ -30,14 +42,14 @@ export default class PauseMiniGameContainer extends Phaser.GameObjects.Container
         currentGUIMiniGame.scene.resume();
       }
 
-      this.pauseImage.setVisible(false);
+      this.pauseModal.setVisible(false);
     } else {
       currentMiniGameScene.scene.pause();
       if(currentGUIMiniGame != null) {
         currentGUIMiniGame.scene.pause();
       }
 
-      this.pauseImage.setVisible(true);
+      this.pauseModal.setVisible(true);
     }
   }
 
