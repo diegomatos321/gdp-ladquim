c074f68114ed3df9366550579810f16acb993544
diegomatos <diegovasconcelos62@gmail.com>
2021-12-16 20:45:11 -0300

    Criando StartGameModal

--- src/Conservacao-Energia/Modals/startGameModal.js
+++ src/Conservacao-Energia/Modals/startGameModal.js
@@ -1,21 +1,20 @@
 import Phaser from "phaser";
-import Button from "../common/scripts/Button";
-import LoadingInterface from "../common/scripts/LoadingInterface";
-import ShowInstrucoes from "../common/scripts/ShowInstrucoes";
-import CONSTANTS from "../GLOBAL_CONSTANTS.json"
+import Button from "../../common/scripts/Button";
+import LoadingInterface from "../../common/scripts/LoadingInterface";
+import ShowInstrucoes from "../../common/scripts/ShowInstrucoes";
+import GAME_CONSTANTS from "../GAME_CONSTANTS.json"
+import GLOBAL_CONSTANTS from "../../GLOBAL_CONSTANTS.json"
 
-export default class InicioMinigame extends Phaser.Scene {
+export default class StartGameModal extends Phaser.Scene {
   constructor() {
-    super({key: CONSTANTS.INICIO_MINI_GAME_QUIMICA_CONSERVACAO});   
+    super({key: GAME_CONSTANTS.START_GAME_MODAL});   
   }
 
   init = () => {
-    const GameManager = this.scene.get(CONSTANTS.GAME_MANAGER);
-    GameManager.setCurrentScene(CONSTANTS.INICIO_MINI_GAME_QUIMICA_CONSERVACAO)
-  }
-
-  preload = () => {
-    new LoadingInterface(this)
+    this.GameManager = this.scene.get(GLOBAL_CONSTANTS.GAME_MANAGER);
+    this.GameManager.setCurrentScene(this.scene.key)
+    
+    this.gameScene = this.scene.get(GLOBAL_CONSTANTS.MINI_GAME_QUIMICA_CONSERVACAO);
   }
 
   create = () => {
@@ -47,26 +46,24 @@ export default class InicioMinigame extends Phaser.Scene {
     
     this.playBtn.on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handlePlayBtn)
     this.voltarBtn.on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handleVoltarBtn)
-    this.events.on(Phaser.Scenes.Events.SHUTDOWN, this.cleanEvents)
+    this.gameScene.events.on(GAME_CONSTANTS.START_GAME, this.cleanAndStop)
+    this.gameScene.events.on(Phaser.Scenes.Events.SHUTDOWN, this.cleanAndStop)
   }
 
   handleVoltarBtn = () => {
-    console.log("Voltar")
-    this.scene.start(CONSTANTS.MAIN_MENU)
+    this.gameScene.events.emit(GAME_CONSTANTS.RETURN_TO_MENU);
   }
 
   handlePlayBtn = () => {
-    console.log("Jogar")
-    this.scene.start(CONSTANTS.MINI_GAME_QUIMICA_CONSERVACAO)
+    this.gameScene.events.emit(GAME_CONSTANTS.START_GAME);
   }
 
-  cleanEvents = () => {
+  cleanAndStop = () => {
     console.log("Cleaning Events from Inicio Minigame")
 
     this.playBtn.removeListener(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handlePlayBtn)
     this.voltarBtn.removeListener(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handleVoltarBtn)
 
-    const GameManager = this.scene.get(CONSTANTS.GAME_MANAGER);
-    GameManager.setCurrentScene(null)
+    this.scene.stop();
   }
 }
