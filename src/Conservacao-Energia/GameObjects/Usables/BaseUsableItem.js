0abd57e08e80616373c8bf578de047db69396e66
diegomatos <diegovasconcelos62@gmail.com>
2022-09-04 16:39:22 -0300

    Alterando comportamento padrão do objeto 'usável'

--- src/Conservacao-Energia/GameObjects/Usables/BaseUsableItem.js
+++ src/Conservacao-Energia/GameObjects/Usables/BaseUsableItem.js
@@ -1,14 +1,36 @@
-export default class BaseUsableItem extends Phaser.GameObjects.Image {
+import crossSceneEventEmitter from "../../../Singletons/CrossSceneEventEmitter";
+import GAME_CONSTANTS from "../../GAME_CONSTANTS.json";
+
+export default class BaseUsableItem extends Phaser.Physics.Arcade.Image {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
 
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this);
-
-        this.setInteractive({
+        
+        /* this.setInteractive({
             draggable: true
-        });
-        this.setState("isDragging", false);
+        }); */
+        this.disableBody();
         this.setDepth(10);
+    }
+
+    followPointer = () => {
+
+    }
+
+    setEnableToPick = (canPick) => {
+        if (canPick) {
+            this.setInteractive();
+            this.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, this.handlePick);
+        } else {
+            this.removeInteractive();
+            this.removeListener(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, this.handlePick);
+        }
+    }
+
+    handlePick = () => {
+        crossSceneEventEmitter.emit(GAME_CONSTANTS.ITEM_PICK_UP, this.name);
+        this.destroy();
     }
 }
