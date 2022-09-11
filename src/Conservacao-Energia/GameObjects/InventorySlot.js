36123f71d82feed41cedcfba5dcc5979a331db54
diegomatos <diegovasconcelos62@gmail.com>
2022-09-11 11:36:21 -0300

    Adicionando label com quantidade do item e alterando comportamento padrão para ficar começar visível

--- src/Conservacao-Energia/GameObjects/InventorySlot.js
+++ src/Conservacao-Energia/GameObjects/InventorySlot.js
@@ -1,21 +1,29 @@
 export default class InventorySlot extends Phaser.GameObjects.Container {
-    usableImage;
+    static slotWidth = 250;
+    static slotHeight = 250;
 
-    constructor(scene, x, y, name) {
+    usableSlot;
+    usableImage;
+    usableLabel;
+
+    constructor(scene, x, y, name, amount) {
         super(scene, x, y);
 
         this.scene.add.existing(this);
 
-        const usableSlot = this.scene.add.image(0, 0, 'slot-inventario');
+        this.usableSlot = this.scene.add.image(0, 0, 'slot-inventario');
         this.usableImage = this.scene.add.image(0, 0, name);
-        this.usableImage.setVisible(false);
+
+        const fontSize = 36;
+        const paddingRight = 26;
+        const paddingBottom = 14;
+        this.usableLabel = this.scene.add.text((InventorySlot.slotWidth / 2) - (fontSize / 2 + paddingRight), (InventorySlot.slotHeight / 2) - (fontSize / 2 + paddingBottom), `x${amount}`, {
+            fontSize
+        }).setOrigin(0.5);
 
         this.setName(name);
+        this.setData('amount', amount);
 
-        this.add([usableSlot, this.usableImage]);
-    }
-
-    setImageVisible = (newState) => {
-        this.usableImage.setVisible(newState);
+        this.add([this.usableSlot, this.usableImage, this.usableLabel]);
     }
 }
